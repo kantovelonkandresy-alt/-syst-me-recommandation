0b2a8703356868d085613523d18db7534e2c1d67
kantovelonkandresy-alt <kantovelonkandresy@gmail.com>
2026-09-14 11:49:45 +0200

    Update graphe.py

--- graphe.py
+++ graphe.py
@@ -7,9 +7,9 @@ import plotly.graph_objects as go
 from matplotlib.patches import Patch
 from donnees import obtenir_donnees
 
-COULEUR_UTILISATEUR = "#E50914"
-COULEUR_FILM = "#B3B3B3"
-COULEUR_ARETE = "#444444"
+COULEUR_UTILISATEUR = "#D4AF37"
+COULEUR_FILM = "#2C7DA0"
+COULEUR_ARETE = "#3A3F4B"
 
 
 def creer_graphe():
@@ -143,7 +143,7 @@ def obtenir_figure_plotly(G):
             mode="markers+text",
             text=noms,
             textposition="top center",
-            textfont=dict(size=11, family="Poppins, sans-serif", color="#E5E5E5"),
+            textfont=dict(size=11, family="Poppins, sans-serif", color="#EDE6D6"),
             hovertext=textes_survol,
             hoverinfo="text",
             name=nom_legende,
@@ -168,20 +168,20 @@ def obtenir_figure_plotly(G):
             y=1.02,
             xanchor="right",
             x=1,
-            font=dict(family="Poppins, sans-serif", size=12, color="#E5E5E5"),
+            font=dict(family="Poppins, sans-serif", size=12, color="#EDE6D6"),
         ),
         margin=dict(l=10, r=10, t=40, b=10),
-        plot_bgcolor="#181818",
-        paper_bgcolor="#181818",
+        plot_bgcolor="#12151F",
+        paper_bgcolor="#12151F",
         xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
         yaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
         height=600,
         hoverlabel=dict(
-            bgcolor="#0A0A0A",
-            bordercolor="#E50914",
+            bgcolor="#0A0E17",
+            bordercolor="#D4AF37",
             font_size=13,
             font_family="Poppins, sans-serif",
-            font_color="#E5E5E5",
+            font_color="#EDE6D6",
         ),
     )
 
